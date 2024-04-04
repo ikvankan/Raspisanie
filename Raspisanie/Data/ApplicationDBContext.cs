7bb682cac8ed010349d074213377e418f253a7c7
ikvankan <ikvankan@gmail.com>
2024-04-04 15:52:13 +0300

    Добавьте файлы проекта.

---
+++ Raspisanie/Data/ApplicationDBContext.cs
@@ -0,0 +1,19 @@
+﻿namespace Raspisanie.Data
+{
+    public class ApplicationDBContext
+    {
+        public class ApplicationDbContext : IdentityDbContext
+        {
+            public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+            {
+
+            }
+            public DbSet<ItemType> ItemType { get; set; }
+            public DbSet<Punkt> Punkt { get; set; }
+            public DbSet<Item> Item { get; set; }
+            public DbSet<ApplicationUser> ApplicationUser { get; set; }
+            public DbSet<OrderHeader> OrderHeader { get; set; }
+            public DbSet<OrderDetail> OrderDetail { get; set; }
+        }
+    }
+}
---
+++ Raspisanie/Models/Teacher.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Raspisanie.Models
+{
+    public class Teacher
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string TeacherName { get; set; }
+        public int AuditoryID { get; set; }
+
+    }
+}
