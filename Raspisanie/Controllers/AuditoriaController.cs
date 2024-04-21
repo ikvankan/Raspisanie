056cce652770ada12d4e0b5368a80018a0962a73
ikvankan <ikvankan@gmail.com>
2024-04-21 15:06:57 +0300

    feet: add CRUD for AuditoriaTable

---
+++ Raspisanie/Controllers/AuditoriaController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Raspisanie.Data;
+using Raspisanie.Models;
+
+namespace Raspisanie.Controllers
+{
+    public class AuditoriaController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public AuditoriaController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Auditoria> objList = _db.Auditoria;
+            return View(objList);
+        }
+
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+        //POST-CREATE
+        [HttpPost]//Action метод типа пост дададада
+        [ValidateAntiForgeryToken]//Токен от взлома
+        public IActionResult Create(Auditoria obj)
+        {
+            _db.Auditoria.Add(obj);
+            _db.SaveChanges();
+            TempData[WC.Success] = "Аудитория создана успешно!";
+            return RedirectToAction("Index");
+        }
+
+        //GET-EDIT
+        public IActionResult Edit(int? Id)
+        {
+            if (Id == null) { return NotFound(); }
+            var obj = _db.Auditoria.Find(Id);
+            if (obj == null) { return NotFound(); }
+            return View(obj);
+        }
+
+        //POST-EDIT
+        [HttpPost]//Action метод типа пост дададада
+        [ValidateAntiForgeryToken]//Токен от взлома
+        public IActionResult Edit(Auditoria obj)
+        {
+            _db.Auditoria.Update(obj);
+            _db.SaveChanges();
+            TempData[WC.Success] = "Аудитория изменена успешно!";
+            return RedirectToAction("Index");
+        }
+
+        //GET-DELETE
+        public IActionResult Delete(int? Id)
+        {
+            if (Id == null) { return NotFound(); }
+            var obj = _db.Auditoria.Find(Id);
+            if (obj == null) { return NotFound(); }
+            return View(obj);
+        }
+
+
+        //POST-DELETE
+        [HttpPost]//Action метод типа пост дададада
+        [ValidateAntiForgeryToken]//Токен от взлома
+        public IActionResult DeletePost(int? Id)
+        {
+            var obj = _db.Auditoria.Find(Id);
+            if (obj == null) { return NotFound(); }
+            _db.Auditoria.Remove(obj);
+            _db.SaveChanges();
+            TempData[WC.Success] = "Аудитория удалена успешно!";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Raspisanie/WC.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Raspisanie
+{
+    public static class WC
+    {
+        public const string ImagePath = @"\images\Item\";
+        public const string SessionCart = "ShopingCartSession";
+
+        public const string AdminRole = "Admin";
+        public const string CustomerRole = "Customer";
+
+        public const string EmailAdmin = "testpochtadlyavsego@gmail.com";
+
+        public const string Success = "Success";
+        public const string Error = "Error";
+
+        public const string StatusPending = "Создан";
+        public const string StatusApproved = "Подтверждён";
+        public const string StatusInProcess = "Собирается";
+        public const string StatusShipped = "Отправлен";
+        public const string StatusCancelled = "Отменён";
+        public const string StatusRefunded = "Возвращение средств";
+
+
+
+        public static readonly IEnumerable<string> ListStatus = new ReadOnlyCollection<string>(
+            new List<string>
+            {
+                StatusApproved,StatusCancelled,StatusInProcess,StatusPending,StatusRefunded,StatusShipped
+            });
+    }
+}
