bc0e1212d807e62f06a3ee541cc055915f7ffe1a
ikvankan <ikvankan@gmail.com>
2024-04-21 18:51:21 +0300

    feet:: add CRUD for TeacherTable

---
+++ Raspisanie/Controllers/TeacherController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Raspisanie.Data;
+using Raspisanie.Models;
+using Raspisanie.Models.ViewModels;
+
+namespace Raspisanie.Controllers
+{
+    public class TeacherController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public TeacherController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Teacher> objList = _db.Teacher;
+            foreach (var obj in objList)
+            {
+                obj.Auditoria = _db.Auditoria.FirstOrDefault(u => u.Id == obj.AuditoryId);
+                
+            }
+            return View(objList);
+        }
+
+        //GET-UPSERT
+        public IActionResult Upsert(int? Id)
+        {
+            //IEnumerable<SelectListItem> ItemTypeDropDown = _db.ItemType.Select(i => new SelectListItem
+            //{
+            //    Text = i.Name,
+            //    Value = i.Id.ToString()
+            //});
+            //ViewBag.ItemTypeDropDown = ItemTypeDropDown;
+            //Item item = new Item();
+
+            TeacherVM teacherVM = new TeacherVM()
+            {
+                Teacher = new Teacher(),
+                AuditoriaSelectList = _db.Auditoria.Select(i => new SelectListItem
+                {
+                    Text = i.AuditoryName,
+                    Value = i.Id.ToString()
+                }),
+            };
+            if (Id == null)
+            {
+                //Создаем новый
+                return View(teacherVM);
+            }
+            else
+            {
+                teacherVM.Teacher = _db.Teacher.Find(Id);
+                if (teacherVM.Teacher == null)
+                {
+                    return NotFound();
+                }
+                return View(teacherVM);
+            }
+
+        }
+        //POST-UPSERT
+        [HttpPost]//Action метод типа пост дададада
+        [ValidateAntiForgeryToken]//Токен от взлома
+        public IActionResult Upsert(TeacherVM teacherVM)
+        {
+            if (teacherVM.Teacher.Id == 0)
+            {
+                _db.Teacher.Add(teacherVM.Teacher);
+                TempData[WC.Success] = "Предмет создан успешно!";
+            }
+            else
+            {
+                //обновляем
+                _db.Teacher.Update(teacherVM.Teacher);
+                TempData[WC.Success] = "Предмет изменён успешно!";
+            }
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //GET-DELETE
+        public IActionResult Delete(int? Id)
+        {
+            if (Id == null) { return NotFound(); }
+            Teacher teacher = _db.Teacher.Include(u => u.Auditoria).FirstOrDefault(u => u.Id == Id);
+            //var obj = _db.Teacher.Find(Id);
+            if (teacher == null) { return NotFound(); }
+            return View(teacher);
+        }
+        //POST-DELETE
+        [HttpPost, ActionName("Delete")]//Action метод типа пост дададада,ну и имя для метода чтобы асп понимал как к нему лучше обращатся
+        [ValidateAntiForgeryToken]//Токен от взлома
+        public IActionResult DeletePost(int? Id)
+        {
+            var obj = _db.Teacher.Find(Id);
+            if (obj == null) { return NotFound(); }
+            _db.Teacher.Remove(obj);
+            _db.SaveChanges();
+            TempData[WC.Success] = "Аудитория удалена успешно!";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Raspisanie/Models/ViewModels/TeacherVM.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Raspisanie.Models.ViewModels
+{
+    public class TeacherVM
+    {
+        public Teacher Teacher { get; set; }
+        public IEnumerable<SelectListItem> AuditoriaSelectList { get; set; }
+    }
+}
