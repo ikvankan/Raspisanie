97a3c7dc489227fd2e375ff434ff54179801d4a6
ikvankan <ikvankan@gmail.com>
2024-04-27 22:35:00 +0300

    feet:: add SimpleGeneration

--- Raspisanie/Controllers/HomeController.cs
+++ Raspisanie/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Raspisanie.Data;
 using Raspisanie.Models;
 using System.Diagnostics;
 
@@ -8,9 +9,14 @@ namespace Raspisanie.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+
+        private readonly ApplicationDbContext _db;
+        
+
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public IActionResult Index()
@@ -28,5 +34,84 @@ namespace Raspisanie.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+
+
+        [HttpPost]
+        public IActionResult Generate(string groupNumber)
+        {
+            IEnumerable<Group> groupList = _db.Group.ToList();
+            IEnumerable<Predmet> predmetList = _db.Predmet.ToList();
+            List<Placement> newSchedules = new List<Placement>();
+
+            // Получение текущей даты
+            string currentDate = DateTime.Now.ToString("dd-MM-yyyy");
+
+            // Создание имени файла
+            string fileName = $"Расписание на {currentDate}.txt";
+
+            // Путь к файлу
+            string path = Path.Combine(@"C:\Users\User\Desktop\DP\Raspisanie\Raspisanie\Files", fileName);
+
+            // Получение Id выбранной группы из базы данных
+            var group = groupList.FirstOrDefault(g => g.Name == groupNumber);
+            if (group == null)
+            {
+                return View(); // или другой вид обработки ошибок
+            }
+            int groupId = group.Id;
+
+            // Генерация случайного числа от 1 до 3
+            
+
+            // Создание записей в зависимости от дня недели
+            int numberOfEntries = DateTime.Now.DayOfWeek == DayOfWeek.Friday ? 4 : 3;
+
+            for (int i = 0; i < numberOfEntries; i++)
+            {
+                Random random = new Random();
+                int randomNumber = i+1;
+                // Выбор случайного предмета
+                int randomIndex = random.Next(predmetList.Count())+1;
+
+                int[] Ids = new int[predmetList.Count()];
+
+
+                int id_index = 0;
+                foreach (var predmet in predmetList)
+                {
+                    Ids[id_index] = predmet.Id;
+                    id_index++;
+                }
+
+                //var randomSubject = predmetList.FirstOrDefault(g=>g.PredmetName == "Физика");
+                
+                int subjectId = Ids[random.Next(0, Ids.Length)];
+
+                // Создание строки для записи в файл
+                string entry = $"Номер группы: {groupNumber}, Дата: {currentDate}, Id группы: {groupId}, Id предмета: {subjectId}, Случайное число: {randomNumber}\n";
+
+                // Добавление строки в файл
+                System.IO.File.AppendAllText(path, entry);
+
+                // Создание нового объекта Schedule и добавление его в базу данных
+                Placement schedule = new Placement
+                {
+                    GroupId = groupId,
+                    PredmetId = subjectId,
+                    Date = DateTime.Now.ToString(),
+                    index = randomNumber
+                };
+                _db.Placement.Add(schedule);
+                newSchedules.Add(schedule);
+            }
+
+            
+            
+
+            return View(newSchedules);
+        }
+
+
     }
 }
